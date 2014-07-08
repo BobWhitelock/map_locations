debd8cd931b835e015d3a5658728de88801a54aa
bob <bob.whitelock1@gmail.com>
2014-07-08 15:40:27 +0100

    added messages indicating progress throughout program

---
+++ config.py
@@ -0,0 +1,13 @@
+READABILITY_PARSER_URL = 'https://www.readability.com/api/content/v1/parser'
+READABILITY_API_TOKEN = 'f3eceed717ac392a24961b43bb4a39bc6d2b83da'
+
+NER_SERVER_PORT = 1234
+NER_SERVER_HOST = 'localhost'
+NER_DEPLOY_SCRIPT = '/media/Storage/stanford-ner-master-dat/ner-deploy.sh'
+
+MYSQL_USERNAME = 'root'
+MYSQL_PASSWORD = 'password'
+MYSQL_DATABASE = 'geonames'
+MYSQL_HOST = 'localhost'
+
+RESULTS_DIRECTORY = '/media/Storage/Python_programs/Dissertation_python3/results/'
